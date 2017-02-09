dd1b9c464479051b0e62ba266083f1f94e101fad
AnLuoRidge <anluoridge@gmail.com>
2017-02-09 19:49:02 +0800

    Initial Commit

---
+++ Copycat of xCurrency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Copycat of xCurrency
+//
+//  Created by AnLuoRidge on 2/9/17.
+//  Copyright © 2017 里脊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
