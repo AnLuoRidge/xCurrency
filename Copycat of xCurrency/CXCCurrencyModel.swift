cc63e6b200e1081b8d7f9cba7dbd7df6a889697a
AnLuoRidge <anluoridge@gmail.com>
2017-02-27 15:37:44 +0800

    edited currency model

--- Copycat of xCurrency/CXCCurrencyModel.swift
+++ Copycat of xCurrency/CXCCurrencyModel.swift
@@ -8,23 +8,6 @@
 
 import UIKit
 
-enum Currency: String  {
-    case CNY = "CNY"
-    case USD = "USD"
-    case AUD = "AUD"
-    case JPY = "JPY"
-    case XAU = "XAU"
-    case XAG = "XAG"
-}
-
-let screenWidth = UIScreen.main.bounds.width
-let screenHeight = UIScreen.main.bounds.height
-let xppi = screenWidth / 375
-let yppi = screenHeight / 667
-let keyboardHeight: CGFloat = CGFloat(280.0.yppi)
-let dateButtonSelectedColor = UIColor(hex: "#29B774")
-let dateButtonNormalColor = UIColor(hex: "#99A4BF")
-
 //extension Float {
 //    static let widthUnit = Float(screenWidth / 375)
 //    static let heightUnit = Float(screenHeight / 667)
@@ -103,11 +86,6 @@ extension Int {
         return CGFloat(self) * Int.heightUnit
     }
 }
-
-let fullNameDict = ["CNY":"China", "USD": "United", "AUD":"Australia", "JPY": "Japan"]
-let symbolDict = ["CNY":"￥", "USD": "$", "AUD":"$", "JPY":"￥"]
-
-var currentCurrencyDict:[String:Float] = ["CNY/USD":1/6, "CNY/AUD":1/4, "CNY/JPY":14, "USD/CNY":6.8665, "USD/AUD":1.2968, "USD/JPY":113.393]
 
 class CXCCurrencyModel: NSObject {
 
